acd233b7e06363361766e761cfb28c8218bca705
Iden Huang <idenhuang@hotmail.com>
2026-02-15 13:12:39 -0800

    Modified ESC class to interface from the ESC driver

--- Propeller Characterization/Propeller Characterization Script/include/ESC.h
+++ Propeller Characterization/Propeller Characterization Script/include/ESC.h
@@ -1,42 +1,120 @@
 #pragma once
 #include <Arduino.h>
 #include <Servo.h>
+#include <esc_motor.h>
 
-class ESC{
+// class ESC{
+//     private:
+//     Servo Esc;
+//     int val = 1000;
+
+//     int pin; //Initialization Values
+//     int minv;
+//     int maxv;
+//     bool initialized = 0;
+
+//     public:
+//     explicit ESC(int _pin, int _min, int _max):pin(_pin), minv(_min), maxv(_max){}
+
+//     bool init(){
+//         pinMode(pin, OUTPUT);
+//         Esc.attach(pin, minv, maxv);
+//         Esc.writeMicroseconds(val);
+//         initialized = 1;
+//         return true;
+//     }
+
+//     void setVal(int _val){
+//         if (!initialized){return;}
+//         val = _val;
+//         Esc.writeMicroseconds(val);
+//     }
+
+//     void incrementVal(int inc){
+//         setVal(val+inc);
+//     }
+//     int getVal(){
+//         if (!initialized){return;}
+//         return val;
+//     }
+//     void stop(){
+//         setVal(1000);
+//     }
+// };
+
+class ESC {
     private:
-    Servo Esc;
-    int val = 1000;
-
-    int pin; //Initialization Values
-    int minv;
-    int maxv;
-    bool initialized = 0;
+    Servo driver;       // The actual Arduino driver
+    esc_motor_t motor;  // The C-struct holding the state
 
     public:
-    explicit ESC(int _pin, int _min, int _max):pin(_pin), minv(_min), maxv(_max){}
+    // Constructor: Initialize the C-struct fields
+    explicit ESC(int _pin, int _min_us, int _max_us) {
+        // Zero out the struct first (good practice)
+        memset(&motor, 0, sizeof(esc_motor_t));
 
-    bool init(){
-        pinMode(pin, OUTPUT);
-        Esc.attach(pin, minv, maxv);
-        Esc.writeMicroseconds(val);
-        initialized = 1;
+        // Map Inputs to Struct
+        motor.channel = _pin;       // We use 'channel' to store the Arduino Pin
+        motor.us_min = _min_us;
+        motor.us_max = _max_us;
+        
+        // specific defaults
+        motor.us_target = 1000;
+        motor.us_output = 1000;
+        motor.armed = false;
+
+        // IMPORTANT: Link the C struct 'hw' pointer to our C++ Servo object
+        // This allows C functions to potentially access the driver if cast back
+        motor.hw = (void*)&driver; 
+    }
+
+    bool init() {
+        // Use the struct data to attach the servo
+        // Note: .hw needs to be cast back to Servo* to be used, 
+        // but here we just use the member 'driver' directly.
+        uint8_t pin = (uint8_t)motor.channel;
+        
+        driver.attach(pin, motor.us_min, motor.us_max);
+        
+        // Set initial state
+        motor.armed = true;
+        setVal(motor.us_min); // Start at 0 throttle
+        
         return true;
     }
 
-    void setVal(int _val){
-        if (!initialized){return;}
-        val = _val;
-        Esc.writeMicroseconds(val);
+    void setVal(int _us_target) {
+        if (!motor.armed) return;
+
+        // 1. Update Target
+        motor.us_target = _us_target;
+        motor.last_cmd_ms = millis(); // Track when we last got a command
+
+        // 2. Safety Clamp (using struct limits)
+        if (motor.us_target < motor.us_min) motor.us_target = motor.us_min;
+        if (motor.us_target > motor.us_max) motor.us_target = motor.us_max;
+
+        // 3. Write to Hardware
+        motor.us_output = motor.us_target;
+        driver.writeMicroseconds(motor.us_output);
     }
 
-    void incrementVal(int inc){
-        setVal(val+inc);
+    void incrementVal(int inc) {
+        // Use the current target from the struct as the base
+        setVal(motor.us_target + inc);
     }
-    int getVal(){
-        if (!initialized){return;}
-        return val;
+
+    int getVal() {
+        if (!motor.armed) return 0;
+        return motor.us_output;
     }
-    void stop(){
-        setVal(1000);
+
+    void stop() {
+        setVal(motor.us_min); // Use the struct's defined minimum
+    }
+
+    // Helper: Expose the raw struct in case C functions need it
+    esc_motor_t* getRawState() {
+        return &motor;
     }
 };
--- Propeller Characterization/Propeller Characterization Script/src/main.cpp
+++ Propeller Characterization/Propeller Characterization Script/src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <esc_motor.h>
 #include "../include/ESC.h"
 #include "HX711.h"
 #include "Servo.h"
@@ -6,98 +7,104 @@
 #define calibration_factor_1 38000
 #define calibration_factor_2 -98000
 
-#define DOUT1  A0
-#define CLK1  A1
-#define DOUT2  A2
-#define CLK2  A3
+#define DOUT1 A0
+#define CLK1 A1
+#define DOUT2 A2
+#define CLK2 A3
 
 HX711 scale1;
 HX711 scale2;
 
-ESC ESCp(9, 1000, 2000); //Esc for the motor in the positive roll axis (motor p)
-ESC ESCn(10, 1000, 2000); //Esc for the motor in the negative roll axis (motor n)
+ESC ESCp(9, 1000, 2000);  // Esc for the motor in the positive roll axis (motor p)
+ESC ESCn(10, 1000, 2000); // Esc for the motor in the negative roll axis (motor n)
+
 
 // Sweep variables initialization
 unsigned long lastStep = 0;
 const unsigned long stepTime = 500;
 
-int maxPulse = 1500; //The maximum value the motors will be allowed to reach.
-int pulseStep = 100; 
-
+int maxPulse = 1500; // The maximum value the motors will be allowed to reach.
+int pulseStep = 100;
 
 void printVals();
 void stop();
 
-void setup() {
- Serial.begin(9600);
- Serial.println("#Motor Calibration Experiment");
+void setup()
+{
+    Serial.begin(9600);
+    Serial.println("#Motor Calibration Experiment");
 
- // Load Cell Calibration
+    // Load Cell Calibration
 
- scale1.begin(DOUT1, CLK1);
- scale1.set_scale(calibration_factor_1); //This value is obtained by using the SparkFun_HX711_Calibration sketch
- scale1.tare(); //Assuming there is no weight on the scale at start up, reset the scale to 0
- Serial.println("#scale 1 tare done");
+    scale1.begin(DOUT1, CLK1);
+    scale1.set_scale(calibration_factor_1); // This value is obtained by using the SparkFun_HX711_Calibration sketch
+    scale1.tare();                          // Assuming there is no weight on the scale at start up, reset the scale to 0
+    Serial.println("#scale 1 tare done");
 
- scale2.begin(DOUT2, CLK2);
- scale2.set_scale(calibration_factor_2); //This value is obtained by using the SparkFun_HX711_Calibration sketch
- scale2.tare(); //Assuming there is no weight on the scale at start up, reset the scale to 0
- Serial.println("#scale 2 tare done");
+    scale2.begin(DOUT2, CLK2);
+    scale2.set_scale(calibration_factor_2); // This value is obtained by using the SparkFun_HX711_Calibration sketch
+    scale2.tare();                          // Assuming there is no weight on the scale at start up, reset the scale to 0
+    Serial.println("#scale 2 tare done");
 
- if(ESCp.init()){
-  Serial.println("#ESCp initialized.");
- }
- if(ESCn.init()){
-  Serial.println("#ESCn initialized.");
- }
-
- delay(10000); // Wait 10 seconds for ESC to initialize
-
- Serial.println("#Readings:");
-
- Serial.println("pulsep,pulsen,f1,f2,f3");  // CSV header only
-}
-
-void loop() {
- unsigned long now = millis();
-
-  if (now - lastStep >= stepTime) {
-    lastStep = now;
-
-    printVals();
-
-    ESCp.incrementVal(pulseStep);
-
-    if (ESCp.getVal() >= maxPulse) {
-      ESCp.setVal(1000);
-      ESCn.incrementVal(pulseStep);
+    if (ESCp.init())
+    {
+        Serial.println("#ESCp initialized.");
+    }
+    if (ESCn.init())
+    {
+        Serial.println("#ESCn initialized.");
     }
 
-    if(ESCp.getVal() >= maxPulse && ESCn.getVal() >= maxPulse){
-      Serial.print("#Experiment Stopped");
-      stop();
+    delay(10000); // Wait 10 seconds for ESC to initialize
+
+    Serial.println("#Readings:");
+
+    Serial.println("pulsep,pulsen,f1,f2,f3"); // CSV header only
+}
+
+void loop()
+{
+    unsigned long now = millis();
+
+    if (now - lastStep >= stepTime)
+    {
+        lastStep = now;
+
+        printVals();
+
+        ESCp.incrementVal(pulseStep);
+
+        if (ESCp.getVal() >= maxPulse)
+        {
+            ESCp.setVal(1000);
+            ESCn.incrementVal(pulseStep);
+        }
+
+        if (ESCp.getVal() >= maxPulse && ESCn.getVal() >= maxPulse)
+        {
+            Serial.print("#Experiment Stopped");
+            stop();
+        }
     }
-  }
-  // safety checks
-
-  
-
-
-
+    // safety checks
 }
 
-void printVals(){
- Serial.print(ESCp.getVal());
- Serial.print(",");
- Serial.print(ESCn.getVal());
- Serial.print(",");
- Serial.print(scale1.get_units(), 3);
- Serial.print(",");
- Serial.println(scale2.get_units(), 3);
+void printVals()
+{
+    Serial.print(ESCp.getVal());
+    Serial.print(",");
+    Serial.print(ESCn.getVal());
+    Serial.print(",");
+    Serial.print(scale1.get_units(), 3);
+    Serial.print(",");
+    Serial.println(scale2.get_units(), 3);
 }
 
-void stop(){
- ESCp.stop();
- ESCn.stop();
- while(true){}
+void stop()
+{
+    ESCp.stop();
+    ESCn.stop();
+    while (true)
+    {
+    }
 }
