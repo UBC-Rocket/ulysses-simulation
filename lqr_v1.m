f58327145fcf98b039896993b9364276f1297ac6
pianoforte <145713027+p14n0f0rt3@users.noreply.github.com>
2026-02-28 22:32:50 -0800

    lqr_v1.1, model is still underactuated

--- lqr_v1.m
+++ lqr_v1.m
@@ -14,9 +14,9 @@ if ~bdIsLoaded(modelFile)
 end
 
 
-spec = operspec(modelFile); % create operation point specs 
+%spec = operspec(modelFile); % create operation point specs 
 
-op = findop(modelFile, spec); % determine operation point
+%op = findop(modelFile, spec); % determine operation point
 
 % Define I/Os of plant
 
@@ -24,16 +24,23 @@ io(1) = linio('Copy_of_root/In1', 1, 'input');
 io(2) = linio('Copy_of_root/In2', 1, 'input');
 io(3) = linio('Copy_of_root/In3', 1, 'input');
 io(4) = linio('Copy_of_root/In4', 1, 'input');
-io(5) = linio('Copy_of_root/Multibody Sim', 1, 'output'); %q
-io(6) = linio('Copy_of_root/Multibody Sim', 2, 'output'); %omega
+%io(5) = linio('Copy_of_root/Multibody Sim', 1, 'output'); %q
+%io(6) = linio('Copy_of_root/Multibody Sim', 2, 'output'); %omega
 %io(7) = linio('Copy_of_root/Multibody Sim', 3, 'output'); %pos
 %io(8) = linio('Copy_of_root/Multibody Sim', 4, 'output'); %mass
-io(7) = linio('Copy_of_root/Multibody Sim', 5, 'output'); %inertia matrix
-io(8) = linio('Copy_of_root/Multibody Sim', 6, 'output'); %l_gim
+%io(7) = linio('Copy_of_root/Multibody Sim', 5, 'output'); %inertia matrix
+io(5) = linio('Copy_of_root/Multibody Sim', 6, 'output'); %l_gim
 
 
-sys = linearize(modelFile, io, op); % state space system
-G = tf(sys);
+%sys = linearize(modelFile, io, op); % state space system
+
+sys = linearize(modelFile, io); % Not using an op
+
+G = tf(sys); % Transfer Function
+H = [sys.A -sys.B*(R\sys.B')
+    -Q -sys.A']; % Hamiltonian matrix assuming that N = 0
+
+
 
 n = size(sys.A, 1);   % number of states
 m = size(sys.B, 2);   % number of inputs
@@ -41,7 +48,7 @@ m = size(sys.B, 2);   % number of inputs
 Q = diag(ones(n,1)); 
 R = 0.1*eye(m); 
 
-[K, S, E] = lqr(sys, Q, R);
+[K, P, E] = lqr(sys, Q, R);
 
 disp(K);
 
